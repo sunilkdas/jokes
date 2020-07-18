345c368acd504ca457c9aca8116751ab8453bac3
Sunil <b311055@iiit-bh.ac.in>
2020-07-18 12:33:21 +0530

    Commiting the initial files

---
+++ jokes.js
@@ -0,0 +1,28 @@
+try {
+  const jokesURL = `https://official-joke-api.appspot.com/random_joke`;
+
+  let jokesObj = {};
+  jokesObj.getJokes = function () {
+    if (jokesURL) {
+      let dataFromAPI = fetch(jokesURL);
+      dataFromAPI.then(response => response.json())
+    
+        .then((json) => {
+          let header = document.getElementById("header");
+          let jokeEle = document.getElementById("jokesetup");
+          let punchlineEle = document.getElementById("punchline");
+          if (header && jokeEle && punchlineEle) {
+            header.innerHTML = "#" + json.id;
+
+            jokeEle.innerHTML =  json.setup;
+
+          punchlineEle.innerHTML =  json.punchline;
+          }
+        });
+        
+    }
+  };
+  window.addEventListener("load", () => jokesObj.getJokes());
+} catch (e) {
+  console.log("Error while fetching jokes ", e);
+}
