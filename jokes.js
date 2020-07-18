f5b48f8bae5d66fe567424dfc6c3c48b37f299d9
Sunil <b311055@iiit-bh.ac.in>
2020-07-18 12:48:54 +0530

    Adding Swipe Feature

--- jokes.js
+++ jokes.js
@@ -1,12 +1,61 @@
 try {
+  var touchstartX = 0;
+  var touchstartY = 0;
+  var touchendX = 0;
+  var touchendY = 0;
+
+  var gesuredZone = document.querySelector("body");
+
+  gesuredZone.addEventListener(
+    "touchstart",
+    function (event) {
+      touchstartX = event.screenX;
+      touchstartY = event.screenY;
+    },
+    false
+  );
+
+  gesuredZone.addEventListener(
+    "touchend",
+    function (event) {
+      touchendX = event.screenX;
+      touchendY = event.screenY;
+      handleGesure();
+    },
+    false
+  );
+
+  function handleGesure() {
+    var swiped = "swiped: ";
+    if (touchendX < touchstartX) {
+        jokesObj.getJokes()    
+    }
+    if (touchendX > touchstartX) {
+        jokesObj.getJokes()    
+        
+    }
+    if (touchendY < touchstartY) {
+        jokesObj.getJokes()    
+      
+    }
+    if (touchendY > touchstartY) {
+        jokesObj.getJokes()    
+      
+    }
+    if (touchendY == touchstartY) {
+      //  jokesObj.getJokes()    
+        
+    }
+  }
   const jokesURL = `https://official-joke-api.appspot.com/random_joke`;
 
   let jokesObj = {};
   jokesObj.getJokes = function () {
     if (jokesURL) {
       let dataFromAPI = fetch(jokesURL);
-      dataFromAPI.then(response => response.json())
-    
+      dataFromAPI
+        .then((response) => response.json())
+
         .then((json) => {
           let header = document.getElementById("header");
           let jokeEle = document.getElementById("jokesetup");
@@ -14,12 +63,11 @@ try {
           if (header && jokeEle && punchlineEle) {
             header.innerHTML = "#" + json.id;
 
-            jokeEle.innerHTML =  json.setup;
+            jokeEle.innerHTML = json.setup;
 
-          punchlineEle.innerHTML =  json.punchline;
+            punchlineEle.innerHTML = json.punchline;
           }
         });
-        
     }
   };
   window.addEventListener("load", () => jokesObj.getJokes());
